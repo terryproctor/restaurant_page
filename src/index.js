009faf405d8896a8bfe7707a9afaf2a5e5c8f18f
terryproctor <terryproctor2015@outlook.com>
2021-05-20 21:57:43 +0100

    divs cfeated with functions

--- src/index.js
+++ src/index.js
@@ -10,20 +10,37 @@ bottom.id = 'bottom';
 content.appendChild(top);
 content.appendChild(bottom);
 
+//create navbar without content
 let navbar = document.createElement('div');
 top.appendChild(navbar);
 let ul = document.createElement('ul');
 ul.id ='navbar'
 navbar.appendChild(ul);
 
-
-for (var i = 0; i < 3; i++) {
+//create the three tabs in navbar
+const createTab = (name) => {
     let newDiv = document.createElement('div');
     newDiv.classList.add('tab');
-    ul.appendChild(newDiv);
-}
+    newDiv.textContent = name;
+    return newDiv;
+};
+const tabs = ['Home', 'Menu', 'Contact'];
+tabs.forEach(x => ul.appendChild(createTab(x)));
 
-let tabs = document.getElementsByClassName('tab');
-tabs[0].textContent = 'Home';
-tabs[1].textContent = 'Menu';
-tabs[2].textContent = 'Contact';
+//create three content sections
+const createSection = (name, display) => {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('section');
+    newDiv.id = name;
+    newDiv.classList.add = 'section';
+    newDiv.style.display = display;
+    return newDiv;
+};
+bottom.appendChild(createSection('homeSection', ''));
+bottom.appendChild(createSection('menuSection', 'none'));
+bottom.appendChild(createSection('contactSection', 'none'));
+
+ul.addEventListener('click', (e) => {
+    console.log(e.target);
+}
+);
